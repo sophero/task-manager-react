50fd7a8e3e2d2c2dc0203b0ed3032940dce20510
Sophia Di <backwardsxmarathon@gmail.com>
2017-12-08 13:08:14 -0500

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Task Manager</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
